4da9638b8c362fd08893c101c215d26fd8e6c791
senengkicau <senengkicau@gmail.com>
2026-08-14 12:46:48 +0700

    Update GATE_BUILD_ID in bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ if not BOT_TOKEN or not CHANNEL_ID:
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 log = logging.getLogger(__name__)
 
-GATE_BUILD_ID = "4tF0Ya53s7YztPLYkBAIK"
+GATE_BUILD_ID = "YeDdTRlUNLLMShCIuRhJV"
 
 HEADERS_GATE = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36",
